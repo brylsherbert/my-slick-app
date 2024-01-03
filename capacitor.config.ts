0a22479a9440ea99f1a1cf7989797e4bf4973abd
brylsherbert <magdadarobryl@gmail.com>
2024-01-03 17:01:38 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'My Slick App',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
